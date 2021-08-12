5fe05febf44d794a89cdaca4929acac23336d548
Ubuntu <intern@Intern.ybc2gr1f3x0uvo2zqfucyu3raa.hx.internal.cloudapp.net>
2021-08-12 08:57:21 +0000

    up

---
+++ src/main/java/com/mural/api/ApiApplication.java
@@ -0,0 +1,35 @@
+package com.mural.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/mural/api")
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+	@GetMapping("/canva/{id}")
+	public String getCanvaInfo(@PathVariable String id) {
+		return String.format("getting canva %s's info", id);
+	}
+
+	@GetUser("/user/{id}")
+	public String getUserInfo(@PathVariable String id) {
+		return String.format("getting user %s's info", id);
+	}
+
+}
