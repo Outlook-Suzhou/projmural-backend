27123f48edc1c24759b531b5205576c6e19bdd1b
ZerQAQ <1075843852@qq.com>
2021-08-26 15:15:44 +0800

    add canvaInfo

--- dao/user.go
+++ dao/user.go
@@ -6,12 +6,18 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type CanvaInfo struct {
+	ID string `bson:"id" json:"id"`
+	Name string `bson:"name" json:"name"`
+	RecentOpen int32 `bson:"recent_open", json:"recent_open"`
+}
+
 type User struct {
 	ID          primitive.ObjectID `bson:"_id,omitempty" json:"-"`
 	MicrosoftId string             `bson:"microsoft_id,omitempty" json:"microsoft_id"`
 	Name        string             `bson:"name,omitempty" json:"name"`
 	Mail        string             `bson:"mail,omitempty" json:"mail"`
-	Canvas      []string           `bson:"canvas,omitempty" json:"canvas"`
+	Canvas      []CanvaInfo           `bson:"canvas,omitempty" json:"canvas"`
 }
 
 func (u User) Bson() bson.D {
